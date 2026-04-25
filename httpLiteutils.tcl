1f4579db2fb0c39a0e655f4df5d3712b965d13bd
Adebola Adesina <adebola@aadesina.com>
2026-04-24 20:50:29 -0700

    Edit: clean up

--- httpLiteutils.tcl
+++ httpLiteutils.tcl
@@ -1,7 +1,7 @@
 package provide httpLiteUtils 1.0
 package require Tcl 8.6
+
 namespace eval ::httpLiteUtils {
-    namespace export setHeaders getHeaders body status input dupKeys 
     variable res_headers {}  
     variable httpLite_statuscode_message [dict create]
     variable tmp_statuscode {
@@ -73,7 +73,8 @@ namespace eval ::httpLiteUtils {
 	510 {not extended}
 	511 {newtwork authentication required}
     }
-    # Stauscode with message built on initialization
+    namespace export setHeaders getHeaders body status input dupKeys 
+    # Stauscodes with message build on initialization
     proc createStatusCodeandMessage {} {
 	variable tmp_statuscode
 	variable httpLite_statuscode_message
@@ -81,30 +82,32 @@ namespace eval ::httpLiteUtils {
 	    dict set httpLite_statuscode_message $k $v 
 	}
     }
- # Response Utilities
- namespace eval ::res {
+    # Response Utilities
+    namespace eval ::res {
 	namespace export status json setHeaders getHeaders end
     }
+    # Compute Response code and messages
     createStatusCodeandMessage
 }
 
 # Send the Response back to the client
-proc ::httpLiteUtils::end {{body ""} {format ""}} {
-     upvar ::private::httpLite_channel channel
-     variable res_headers
-    puts "INSIDE HTTPLITEUTILS::END: PRIVATE-CHN:$channel \
-RESPONSE->HEADERS: $res_headers"
-    if {$body ne ""} {
+proc ::httpLiteUtils::end {{payload ""} {fmt ""}} {
+    upvar ::private::httpLite_channel channel
+    variable res_headers
+    #puts "INSIDE HTTPLITEUTILS::END: PRIVATE-CHN:$channel RESPONSE->HEADERS: $res_headers"
+    if {$payload ne ""} {
+	set transform_res_payload [::httpLiteUtils::payload $payload $fmt]
 	httpLiteSendResHeaders $channel $res_headers
-	set transform_res_body [::httpLiteUtils::body $body $format]
         puts $channel ""
-        puts $channel $transform_res_body
+	fconfigure $channel -translation [expr {$fmt eq "binary" ? $fmt : "auto"}]
+        expr {$fmt eq "binary" ? [puts -nonewline $channel $transform_res_payload] : [puts $channel $transform_res_payload]} 
         flush $channel
     } else {
-      httpLiteSendResHeaders $channel $res_headers
-      puts $channel "" 
-      flush $channel
+	httpLiteSendResHeaders $channel $res_headers
+	puts $channel "" 
+	flush $channel
     }
+    return 0
 }
 
 # User interface [res::setHeadaders] is
@@ -113,13 +116,12 @@ proc ::httpLiteUtils::setHeaders {headers_dict} {
     variable res_headers
     # Set headers for the current response
     foreach {k v} $headers_dict {
-	puts "Key:$k => Value:$v"
 	if {[dict exists $res_headers $k]} {
 	    puts "WARNING:KEY $k exist and will be overwriten"
 	    # Give the user a way to arbot or continue
 	    set res_headers [dict merge $res_headers [dict create $k $v]]
 	}
-	set res_headers [dict merge $res_headers [dict create $k $v]]
+	set res_headers [dict merge $res_headers $headers_dict]
     }
     return $res_headers
 }
@@ -133,34 +135,29 @@ proc ::httpLiteUtils::getHeaders {} {
     # that is set through <::utils::setResHeaders>
     return $res_headers
 }
-
+# Utility for sending response header
 proc ::httpLiteSendResHeaders {channel res_headers} {
     # This need to be called internally after all headers
     # has been set
     #set res_headers ::httpLite::Utils::res_headers
     #upvar ::private::httpLite_channel channel
     if {[dict exists $res_headers res_line]} {
-	 set res_line [dict get $res_headers res_line]
-	 puts "SENDING RESPONSE: $res_line"
-	 puts $channel $res_line
+	set res_line [dict get $res_headers res_line]
+	#puts "SENDING RESPONSE: $res_line"
+	puts $channel $res_line
     } else {
 	error "Please use the command \[httpLiteUtils::status \
-<args statu_code> ?status_message \
-to set the RESPONSE-LINE"
+	<args statu_code> ?status_message \
+	to set the RESPONSE-LINE"
     }
     foreach {k v} $res_headers {
 	if {$k eq "res_line"} continue
-	puts "SENDING RESPONSE: {$k:$v}"
-	puts $channel [format "%s: %s" $k $v]
+	#puts "SENDING RESPONSE: {$k:$v}"
+	#puts $channel [format "%s: %s" $k $v]
     }
 }
 
-### User interface res message
-# Overload to set the status code message or one shot
-# to attached a message to a statuscode
-#proc ::utils::message {}
-
-# User interface res status
+# Utility for sending response status
 proc ::httpLiteUtils::status {status_code {message ""}} {
     variable httpLite_statuscode_message
     variable channel
@@ -168,20 +165,21 @@ proc ::httpLiteUtils::status {status_code {message ""}} {
     set status_message [expr {[dict exists $httpLite_statuscode_message $status_code] ? \
 				  [dict get $httpLite_statuscode_message $status_code]:$message}]
     puts [httpLiteUtils::setHeaders [dict create res_line [format "HTTP/1.1 %s %s" $status_code $status_message]]]
+    return 0
 }
 
-# User interface res body
-proc ::httpLiteUtils::body {res_body {transform "stringify"}} {
-    set transform_res_body ""
+# Utility for sending response payload/data
+proc ::httpLiteUtils::payload {res_payload {transform "stringify"}} {
+    set transform_res_payload ""
     switch $transform {
 	"json" {
-	    return  [::httpLiteUtilsJson $res_body]
+	    return  [::httpLiteUtilsJson $res_payload]
 	}
 	"binary" {
-	    return [::httpLiteUtilsBinary $res_body]
+	    return [::httpLiteUtilsBinary $res_payload]
 	}
 	default  {
-	    return [::httpLiteUtilsStringify $res_body]
+	    return [::httpLiteUtilsStringify $res_payload]
 	}
 	
     }
@@ -202,23 +200,24 @@ proc ::httpLiteUtilsBinary {binary_obj} {
 proc ::httpLiteUtilsJson {json_str} {
     #TODO: find a way to type the parameter<json_str>
     # and do amazing transformation to the data before dispatching
-   return $json_str
+    return $json_str
 }
 
 
 proc ::res::status {status_code {message ""}} {
-    ::httpLiteUtils::status $status_code $message
+     ::httpLiteUtils::status $status_code $message
+    return 0
 }
 
 
-proc ::res::end {res_body {body_format ""}} {
-    ::httpLiteUtils::end $res_body $body_format
+proc ::res::end {res_payload {payload_format ""}} {
+    ::httpLiteUtils::end $res_payload $payload_format
     return 0
 }
 
 proc ::res::json  {json_str} {
-	return httpLiteUtilsJson  $json_str
-       
+    return httpLiteUtilsJson  $json_str
+    
 }
 
 proc ::res::setHeaders {dict_header} {
@@ -226,7 +225,7 @@ proc ::res::setHeaders {dict_header} {
 }
 
 proc ::res::getHeaders {{h_key ""}} {
-	
+    
     return [::httpLiteUtils::getHeaders $h_key]
 }
 
@@ -243,7 +242,7 @@ proc ::httpLiteUtils::notify {msg {type ""} {flags ""}} {
     proc checkFlags {type} {
 	upvar flags flags
 	upvar msg msg
-
+	
 	switch $flags {
 	    "" {
 		puts stderr [format "$type: %s" $msg]
@@ -280,6 +279,7 @@ proc ::httpLiteUtils::notify {msg {type ""} {flags ""}} {
     }
 }
 
+
 proc ::httpLiteUtils::input {msg {type ""} {fmt ""} args} {
     ::httpLiteUtils::notify $msg $type $fmt
     set advise [gets stdin]
@@ -300,9 +300,10 @@ proc ::httpLiteUtils::dupKeys {msg type} {
     if {$advise eq "u" || $advise eq "update"} {
 	lappend midw $cb
 	incr midw_len
-	puts $midw_len 
-	puts "MIDDLEWARE: $midw"
+	#puts $midw_len 
+	#puts "MIDDLEWARE: $midw"
     } elseif {$advise eq "a" || $advise eq "abort"} {
-	puts "Aborting!!"
+	#puts "Aborting!!"
     }    
 } 
+
